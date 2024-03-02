eb7d0673339fd629051cb79dea7c0d8d60c8d88e
NavneetNandann <143546892+NavneetNandann@users.noreply.github.com>
2024-03-02 09:08:39 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Navneet"
+
+console.log(typeof score);
+
+let variableInNumber = Number(score)
+
+console.log(typeof variableInNumber)
+console.log(variableInNumber)
+
+// "33" = 33
+// "33abe" = NaN
+// true = 1; false = 0
+
+let isLoggedIn = 1
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 = true;
+// 0 = false;
+// "hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
